e22886d37bd48a0a569508a80f0430c8847665e3
Drew <sirampr@gmail.com>
2024-06-25 12:01:20 -0400

    use dev vars

--- src/index.tsx
+++ src/index.tsx
@@ -3,8 +3,8 @@ import { Hono } from "hono";
 import { env } from "hono/adapter";
 
 type Env = {
-  UPSTASH_REDIS_REST_URL: string;
-  UPSTASH_REDIS_REST_TOKEN: string;
+	UPSTASH_REDIS_REST_URL: string;
+	UPSTASH_REDIS_REST_TOKEN: string;
 };
 
 const app = new Hono();
@@ -26,11 +26,11 @@ app.get("/", (c) => {
 });
 
 app.get("/board", async (c) => {
-  const vars = env<Env>(c);
-  const redis = Redis.fromEnv(vars);
-  await redis.set("board", "foo bar");
-  const board = await redis.get("board");
-  return c.text(board as string);
+	const vars = env<Env>(c);
+	const redis = Redis.fromEnv(vars);
+	await redis.set("board", "foo bar");
+	const board = await redis.get("board");
+	return c.text(board as string);
 });
 
 export default app;

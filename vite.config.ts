10df72ebe764643f5b325dd3669c891b63f8a83c
J. Lewis <6710419+lewxdev@users.noreply.github.com>
2024-06-25 10:05:50 -0400

    chore: configure prettier and formatting

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,12 @@
-import { defineConfig } from 'vite'
-import devServer from '@hono/vite-dev-server'
-import cloudflareAdapter from '@hono/vite-dev-server/cloudflare'
+import { defineConfig } from "vite";
+import devServer from "@hono/vite-dev-server";
+import cloudflareAdapter from "@hono/vite-dev-server/cloudflare";
 
 export default defineConfig({
-  plugins: [
-    devServer({
-      adapter: cloudflareAdapter,
-      entry: 'src/index.ts',
-    }),
-  ],
-})
+	plugins: [
+		devServer({
+			adapter: cloudflareAdapter,
+			entry: "src/index.ts",
+		}),
+	],
+});

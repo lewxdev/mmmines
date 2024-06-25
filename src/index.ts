0b37be7d0490a83052308cf4569b2e01cdb36486
Drew <sirampr@gmail.com>
2024-06-25 11:09:50 -0400

    redis test

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,24 @@
-import { Hono } from 'hono'
+import { Redis } from "@upstash/redis/cloudflare";
+import { Hono } from "hono";
+import { env } from "hono/adapter";
 
-const app = new Hono()
+type Env = {
+  UPSTASH_REDIS_REST_URL: string;
+  UPSTASH_REDIS_REST_TOKEN: string;
+};
 
-app.get('/', (c) => {
-  return c.text('Hello Cloudflare Workers!')
-})
+const app = new Hono();
 
-export default app
+app.get("/", (c) => {
+  return c.text("Hello Cloudflare Workers!");
+});
+
+app.get("/board", async (c) => {
+  const vars = env<Env>(c);
+  const redis = Redis.fromEnv(vars);
+  await redis.set("board", "foo bar");
+  const board = await redis.get("board");
+  return c.text(board as string);
+});
+
+export default app;

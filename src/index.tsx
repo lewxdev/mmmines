bf41258a130967f935afe60d2e90bc5c2c3f5a60
Drew Radcliff <sirampr@gmail.com>
2024-06-25 13:38:26 -0400

    feat: configure upstash redis (#1)
    * redis test
    
    * use dev vars
    
    * fix: specify runtime
    
    Co-authored-by: J. Lewis <6710419+lewxdev@users.noreply.github.com>
    
    * refactor: destructure env vars
    
    ---------
    
    Co-authored-by: J. Lewis <6710419+lewxdev@users.noreply.github.com>

--- src/index.tsx
+++ src/index.tsx
@@ -1,4 +1,11 @@
+import { Redis } from "@upstash/redis/cloudflare";
 import { Hono } from "hono";
+import { env } from "hono/adapter";
+
+type Env = {
+	UPSTASH_REDIS_REST_URL: string;
+	UPSTASH_REDIS_REST_TOKEN: string;
+};
 
 const app = new Hono();
 
@@ -16,6 +23,20 @@ app.get("/", (c) => {
 			</body>
 		</html>,
 	);
+});
+
+app.get("/board", async (c) => {
+	const { UPSTASH_REDIS_REST_URL, UPSTASH_REDIS_REST_TOKEN } = env<Env>(
+		c,
+		"workerd",
+	);
+	const redis = Redis.fromEnv({
+		UPSTASH_REDIS_REST_URL,
+		UPSTASH_REDIS_REST_TOKEN,
+	});
+	await redis.set("board", "foo bar");
+	const board = await redis.get("board");
+	return c.text(board as string);
 });
 
 export default app;

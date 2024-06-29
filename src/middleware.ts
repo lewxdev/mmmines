ed15f1d17abe4d7009ec64180f733f8d1fd9db55
J. Lewis <6710419+lewxdev@users.noreply.github.com>
2024-06-28 20:49:28 -0400

    fix: build with patched redis lib

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,17 +1,16 @@
 import { env } from "hono/adapter";
 import { createMiddleware } from "hono/factory";
-import Redis from "ioredis";
+import { Redis } from "@upstash/redis/cloudflare";
 
 type Env = {
-	UPSTASH_REDIS_URI: string;
+	UPSTASH_REDIS_REST_URL: string;
+	UPSTASH_REDIS_REST_TOKEN: string;
 };
 
 export const redis = () =>
 	createMiddleware(async (c, next) => {
-		const { UPSTASH_REDIS_URI } = env<Env>(c, "workerd");
-		const redis = new Redis(UPSTASH_REDIS_URI);
-
-		c.set("redis", redis);
-
+		const { UPSTASH_REDIS_REST_URL: url, UPSTASH_REDIS_REST_TOKEN: token } =
+			env<Env>(c, "workerd");
+		c.set("redis", new Redis({ url, token }));
 		await next();
 	});

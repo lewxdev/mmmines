6628e1dd098b1dbe6e38d6ece073443017766520
J. Lewis <6710419+lewxdev@users.noreply.github.com>
2024-07-15 16:06:26 -0400

    fix: resolve pr comments and junk

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,25 @@
 "use client";
 
-import { useEffect, useState } from "react";
-import { socket } from "./socket";
 import useSocket from "@/hooks/useSocket";
+import { useEffect, useState } from "react";
+import { Field } from "./game";
+import { socket } from "./socket";
+
+const GRID_SIZE = 20;
+const field = new Field();
+
+const colorMap = new Map<number, string>([
+  [0, "bg-gray-50"],
+  [1, "bg-gray-100"],
+  [2, "bg-gray-200"],
+  [3, "bg-gray-300"],
+  [4, "bg-gray-400"],
+  [5, "bg-gray-500"],
+  [6, "bg-gray-600"],
+  [7, "bg-gray-700"],
+  [8, "bg-gray-800"],
+  [9, "bg-red-500"],
+]);
 
 export default function Page() {
   const [text, setText] = useState("");
@@ -44,6 +61,20 @@ export default function Page() {
         />
         <button type="submit">submit</button>
       </form>
+      <div
+        style={{
+          display: "grid",
+          gap: `${GRID_SIZE * 0.1}px`,
+          gridTemplateColumns: `repeat(${field.width}, ${GRID_SIZE}px)`,
+          gridTemplateRows: `repeat(${field.height}, ${GRID_SIZE}px)`,
+        }}
+      >
+        {field["data"].map((n, index) => {
+          const value = n % 10;
+          const color = colorMap.get(value)!;
+          return <div className={color} key={index} />;
+        })}
+      </div>
     </div>
   );
 }

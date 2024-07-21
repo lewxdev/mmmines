c71b16a12b657c327f71c5fdaf0aef5c122c4378
J. Lewis <6710419+lewxdev@users.noreply.github.com>
2024-07-21 14:47:07 -0700

    feat: add completion handler (#13)
    Co-authored-by: Drew Radcliff <drewradcliff@users.noreply.github.com>

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,9 @@
 import { Field } from "@/components/field";
 
 export default function App() {
-  return <Field />;
+  return (
+    <div className="flex h-screen items-center justify-center">
+      <Field />
+    </div>
+  );
 }

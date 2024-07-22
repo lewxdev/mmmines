aec2fefe2b91e6de726bee69921f4cbabdebb26a
Drew <sirampr@gmail.com>
2024-07-21 23:02:56 -0400

    pr updates

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,13 @@
 import type { Metadata } from "next";
 import { GeistSans } from "geist/font/sans";
 import "@/globals.css";
-import { ClerkProvider, SignedIn, SignOutButton } from "@clerk/nextjs";
+import {
+  ClerkProvider,
+  SignedIn,
+  SignedOut,
+  SignInButton,
+  SignOutButton,
+} from "@clerk/nextjs";
 
 export const metadata: Metadata = {
   title: "mmmines",
@@ -12,15 +18,17 @@ export default function RootLayout(props: Readonly<React.PropsWithChildren>) {
     <ClerkProvider>
       <html lang="en">
         <body className={GeistSans.className}>
-          <div className="p-10">
-            <h1 className="text-4xl">mmmines</h1>
-            <SignedIn>
-              <SignOutButton />
-            </SignedIn>
-            <div className="pb-10" />
-            <div className="flex flex-col items-center justify-center">
-              {props.children}
+          <div className="mx-auto max-w-fit">
+            <div className="p-4">
+              <h1 className="text-4xl">mmmines</h1>
+              <SignedOut>
+                <SignInButton />
+              </SignedOut>
+              <SignedIn>
+                <SignOutButton />
+              </SignedIn>
             </div>
+            <div className="overflow-auto">{props.children}</div>
           </div>
         </body>
       </html>

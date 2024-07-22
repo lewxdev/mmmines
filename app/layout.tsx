ca8f05e02f4acdbea131e5c8f92b7a28d6696094
Drew <sirampr@gmail.com>
2024-07-21 21:11:00 -0400

    feat: add auth

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { GeistSans } from "geist/font/sans";
 import "@/globals.css";
+import { ClerkProvider, SignedIn, SignOutButton } from "@clerk/nextjs";
 
 export const metadata: Metadata = {
   title: "mmmines",
@@ -8,8 +9,21 @@ export const metadata: Metadata = {
 
 export default function RootLayout(props: Readonly<React.PropsWithChildren>) {
   return (
-    <html lang="en">
-      <body className={GeistSans.className}>{props.children}</body>
-    </html>
+    <ClerkProvider>
+      <html lang="en">
+        <body className={GeistSans.className}>
+          <div className="p-10">
+            <h1 className="text-4xl">mmmines</h1>
+            <SignedIn>
+              <SignOutButton />
+            </SignedIn>
+            <div className="pb-10" />
+            <div className="flex flex-col items-center justify-center">
+              {props.children}
+            </div>
+          </div>
+        </body>
+      </html>
+    </ClerkProvider>
   );
 }

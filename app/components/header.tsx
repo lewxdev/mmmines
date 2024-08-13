8bf1dd8ded384d8151d62c63c5e6500c086994d5
J. Lewis <6710419+lewxdev@users.noreply.github.com>
2024-08-13 18:47:12 -0400

    refactor: make styling more concise

--- app/components/header.tsx
+++ app/components/header.tsx
@@ -3,32 +3,21 @@
 import { Percent, UsersIcon } from "lucide-react";
 import { useSocketEvent } from "@/hooks/use-socket-event";
 
-export const HEADER_HEIGHT = 64;
-
 export function Header() {
   const [clientsCount] = useSocketEvent("clientsCount");
   const [exposedPercent] = useSocketEvent("exposedPercent");
 
   return (
-    <header
-      className="w-[min(var(--field-size),100%)] bg-white"
-      style={{ height: HEADER_HEIGHT }}
-    >
-      <div className="flex justify-between">
-        <h1 className="text-4xl">mmmines</h1>
-        <div className="flex flex-col items-end gap-2">
-          {!!clientsCount && (
-            <div className="flex items-center gap-2">
-              <span>{clientsCount}</span>
-              <UsersIcon className="h-4 w-4" />
-            </div>
-          )}
-          {!!exposedPercent && (
-            <div className="flex items-center justify-end gap-2">
-              <span>{exposedPercent}</span>
-              <Percent className="h-4 w-4" />
-            </div>
-          )}
+    <header className="w-[min(var(--field-size),100%)] bg-white px-4 flex justify-between">
+      <h1 className="text-4xl font-extrabold uppercase italic">mmmines</h1>
+      <div className="flex flex-col items-end gap-2">
+        <div className="flex items-center gap-2">
+          <span>{clientsCount ?? "⋯"}</span>
+          <UsersIcon className="h-4 w-4" />
+        </div>
+        <div className="flex items-center gap-2">
+          <span>{exposedPercent ?? "⋯"}</span>
+          <Percent className="h-4 w-4" />
         </div>
       </div>
     </header>

c1d4bd483375bb8d81eccf88d0e54a5a1092c9ff
J. Lewis <6710419+lewxdev@users.noreply.github.com>
2024-08-20 21:49:59 -0400

    fix: usage of redis helpers

--- app/utils/redis.ts
+++ app/utils/redis.ts
@@ -4,33 +4,42 @@ import type { SessionState } from "@/types";
 const fieldKey = "field:data";
 const sessionKey = "user:sessions";
 
-const redis = new Redis(process.env["REDIS_URL"]!, { family: 6 });
+const createHelper =
+  <TArgs extends unknown[], TReturn>(
+    callback: (redis: Redis, ...args: TArgs) => TReturn,
+  ) =>
+  (...args: TArgs) => {
+    const redis = new Redis(process.env["REDIS_URL"]!, { family: 6 });
+    return callback(redis, ...args);
+  };
 
-export async function decodeData() {
+export const decodeData = createHelper(async (redis) => {
   const value = await redis.get(fieldKey);
   if (typeof value !== "string" || !value) {
     throw new Error(`unexpected data on key: ${fieldKey}`);
   }
   return Array.from(value, (char) => char.charCodeAt(0));
-}
+});
 
-export async function encodeData(value: number[]) {
+export const encodeData = createHelper(async (redis, value: number[]) => {
   const binaryString = Buffer.from(value).toString("binary");
   await redis.set(fieldKey, binaryString);
   return value;
-}
+});
 
-export async function getSession(id: string): Promise<SessionState | null> {
+export const getSession = createHelper(async (redis, id: string) => {
   const value = await redis.hget(sessionKey, id);
   return value === "alive" || value === "dead" ? value : null;
-}
+});
 
-export async function setSession(id: string, state: SessionState) {
-  return redis.hset(sessionKey, id, state);
-}
+export const setSession = createHelper(
+  async (redis, id: string, state: SessionState) => {
+    return redis.hset(sessionKey, id, state);
+  },
+);
 
-export async function resetSessions() {
+export const resetSessions = createHelper(async (redis) => {
   const sessions = await redis.hkeys(sessionKey);
   const args = sessions.flatMap((sessionId) => [sessionId, "alive"]);
-  redis.hset(sessionKey, ...args);
-}
+  return redis.hset(sessionKey, ...args);
+});

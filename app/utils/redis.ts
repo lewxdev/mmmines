efe43b1cc4c8d1bf1d4594da8f013c47dc1af1df
J. Lewis <6710419+lewxdev@users.noreply.github.com>
2024-08-20 22:02:34 -0400

    refactor: okay, maybe a little more readable

--- app/utils/redis.ts
+++ app/utils/redis.ts
@@ -4,42 +4,40 @@ import type { SessionState } from "@/types";
 const fieldKey = "field:data";
 const sessionKey = "user:sessions";
 
-const createHelper =
-  <TArgs extends unknown[], TReturn>(
-    callback: (redis: Redis, ...args: TArgs) => TReturn,
-  ) =>
-  (...args: TArgs) => {
-    const redis = new Redis(process.env["REDIS_URL"]!, { family: 6 });
-    return callback(redis, ...args);
-  };
+function getClient() {
+  return new Redis(process.env["REDIS_URL"]!, { family: 6 });
+}
 
-export const decodeData = createHelper(async (redis) => {
+export async function decodeData() {
+  const redis = getClient();
   const value = await redis.get(fieldKey);
   if (typeof value !== "string" || !value) {
     throw new Error(`unexpected data on key: ${fieldKey}`);
   }
   return Array.from(value, (char) => char.charCodeAt(0));
-});
+}
 
-export const encodeData = createHelper(async (redis, value: number[]) => {
+export async function encodeData(value: number[]) {
+  const redis = getClient();
   const binaryString = Buffer.from(value).toString("binary");
   await redis.set(fieldKey, binaryString);
   return value;
-});
+}
 
-export const getSession = createHelper(async (redis, id: string) => {
+export async function getSession(id: string) {
+  const redis = getClient();
   const value = await redis.hget(sessionKey, id);
   return value === "alive" || value === "dead" ? value : null;
-});
+}
 
-export const setSession = createHelper(
-  async (redis, id: string, state: SessionState) => {
-    return redis.hset(sessionKey, id, state);
-  },
-);
+export async function setSession(id: string, state: SessionState) {
+  const redis = getClient();
+  return redis.hset(sessionKey, id, state);
+}
 
-export const resetSessions = createHelper(async (redis) => {
+export async function resetSessions() {
+  const redis = getClient();
   const sessions = await redis.hkeys(sessionKey);
   const args = sessions.flatMap((sessionId) => [sessionId, "alive"]);
   return redis.hset(sessionKey, ...args);
-});
+}
